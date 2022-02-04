8b36cc32a9cae0cc5dde582727801bcd103ad6a4
root <root@localhost>
2022-02-04 00:01:24 +0000

    added unit/integration tests for new attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Price', with: 10.99
+    fill_in 'Date', with: '2022-02-02'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content(10.99)
+    expect(page).to have_content('2022-02-02')
   end
 end
 
